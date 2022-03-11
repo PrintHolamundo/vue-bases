1f74b77c6a8af4094deb0560d1207facda299e67
PrintHolamundo <jdohca@gmail.com>
2022-03-11 00:24:06 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bases de Vue.js
+
+Esto es parte de mi curso de Vue.js
---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+ 
+    template: `
+    <h1>Hola Mundo</h1>
+    <p>Desde app.js</p>
+    `
+    
+})
+
+
+app.mount('#myApp')
+
+
+
+
