08e4f6625543614974d4f51eb65c1ba44ce13548
PrintHolamundo <jdohca@gmail.com>
2022-03-11 11:09:41 -0600

    Eventos

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const app = Vue.createApp({
- 
+
     // template: `
     // <h1>Hola Mundo</h1>
     // <p>  Desde app.js </p>
     // `
-   
-    data(){
+
+    data() {
         return {
-           quote: "im batman",
-           author: "Bruce Wayne"
+            quote: "im batman",
+            author: "Bruce Wayne"
         }
+    },
+    methods: {
+        changeQuote() {
+            console.log("Hola mundo")
+            this.author = "Juan Perez"
+            this.capitalize()
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
+        }
+
     }
-    
+
 })
 
 
